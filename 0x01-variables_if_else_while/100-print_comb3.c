c24e5c510204433d749bae7ef834447be215308f
Gmwongela <gvidla73@gmail.com>
2023-03-20 07:08:53 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,24 @@
  */
 int main(void)
 {
-	int n;
+	int digit1, digit2;
 
-	for (n = 01; n <= 99; n++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar(n + 48);
-		putchar(',');
-		putchar(' ');
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+
 	}
 	putchar('\n');
+
 	return (0);
 }
