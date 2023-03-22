a0a398cd9b51a2126527e87ebb99c1a7349738d8
Gmwongela <gvidla73@gmail.com>
2023-03-22 07:09:50 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,14 @@
 /**
  * main - Entry point
  *
- * Retun: 0
+ * Return: 0
  */
 void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		_putchar(ch);
-	}
 
 	_putchar('\n');
 }
