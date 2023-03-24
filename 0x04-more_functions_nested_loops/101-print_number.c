5e0c5d03fbcbec924a0796b9cfb2a818033f57a1
Gmwongela <gvidla73@gmail.com>
2023-03-24 13:46:53 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputted integer
+ */
+void print_number(int n)
+{
+	int n;
+
+	_ putchar("The integer is:%d\n",n);
+
+	return (0);
+}
