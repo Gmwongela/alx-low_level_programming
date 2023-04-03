106d62af62884e71e413b0ecb5a4a6e587582698
Gmwongela <gvidla73@gmail.com>
2023-04-03 14:04:18 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @dest: pointer to memory area to fill
+ * @n: the number of bytes to fill
+ * @src: constant byte
+ *
+ * Return: Always 0 (success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src;
+	return (s);
+}
